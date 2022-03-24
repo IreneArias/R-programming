ba3a7615e53e0c4dafaa6eef3a1bb4c699cb0f28
IreneArias <97808533+IreneArias@users.noreply.github.com>
2022-03-24 09:12:49 -0400

    Add files via upload

---
+++ Module10.R
@@ -0,0 +1,13 @@
+Package: EZ_Matrix
+Title: Allows for easy use of matrices in datasets 
+Version: 0.0.0.9000
+Authors@R. "Irene Arias Gutierrez iariasgutierrez@usf.edu [aut, cre]" 
+Description: This package will allow for easy Matrix operations necessary during dataset analysis in Bioinformatics.
+    Some of the matrix operations that this package will include are: Inverse of a matrix, addition, substraction,
+    and substition. The reasoning behind this package is that Bioinformaticians are in the constant need of generating
+    matrices to represent data. This usually are represented through contact maps, heat maps along other types of matrix 
+    vizualitation methods. The idea is to develop a package that will help to deal with matrices in an easier manner than 
+    it is already currently done
+Depends: R (>= 3.1.2)
+License: CCO
+LazyData: true 
